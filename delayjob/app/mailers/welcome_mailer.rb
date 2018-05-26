3db0f2725647439777d4f17768d78b08676832e7
Jhon Sedano <jasedanod@unal.edu.co>
2018-05-25 22:30:42 -0500

    Added delayed job exercise

---
+++ delayjob/app/mailers/welcome_mailer.rb
@@ -0,0 +1,13 @@
+class WelcomeMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.welcome_mailer.notify.subject
+  #
+  def notify(person)
+
+    @person = person
+    mail to: person.email, subject: "Welcome"
+  end
+end
